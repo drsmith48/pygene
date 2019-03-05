5d6b9fc205e68824acfa6c372c974e4b787be692
David R. Smith <drsmith8@wisc.edu>
2019-03-04 20:54:07 -0500

    precommit hook

--- pygene/main.py
+++ pygene/main.py
@@ -295,8 +295,7 @@ class GeneLinearScan(_GeneBaseClass):
         index=True
         if self.scanlog and self.params['scan_dims']==1:
             xdata = data[self.scanlog['paramname']]
-        else: ######
-            print('test')
+        else:
             xdata = self.scans
         axes[0].plot(xdata, data['omi'], '-x', label=self.label)
         axes[1].plot(xdata, data['omr'], '-x', label=self.label)
